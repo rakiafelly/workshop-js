a800324af35aa6e406ed9baa88d86cd9f6d2cabf
rakiafelly <rakiafelly14@gmail.com>
2022-03-24 15:37:01 +0100

    creation d'un CRUD des produits

--- commerce/js/commerce.js
+++ commerce/js/commerce.js
@@ -2,7 +2,6 @@ const nom = document.getElementById("name");
 const prix = document.getElementById("prix");
 const envoiButton = document.getElementById("envoi");
 const listBox = document.getElementById("listBox");
-const saveInd = document.getElementById("saveIndex");
 var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
 
 envoiButton.addEventListener("click", add)
@@ -16,18 +15,5 @@ function add() {
   prix.value = "";
   localStorage.setItem("commerce", JSON.stringify(commerce));
 window.location.href='cosmetic.html'
-
-}
-
-function deletecommerce (ind) {
-  var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
-
-  commerce.splice(ind.name, 1);
-  commerce.splice(ind.price,1);
-  localStorage.setItem("commerce", JSON.stringify(commerce));
-  window.location.href='cosmetic.html'
-}
-
-function update(){
 
 }

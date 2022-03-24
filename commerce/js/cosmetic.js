15faa25453cc5594546fbff0645a3295294a5f39
rakiafelly <rakiafelly14@gmail.com>
2022-03-24 15:36:10 +0100

    commit

--- commerce/js/cosmetic.js
+++ commerce/js/cosmetic.js
@@ -2,19 +2,16 @@ function display(){
     let htmlCode = "";
     var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
 
-      commerce.forEach(x => {
-        htmlCode += `<div class='flex mb-4 items-center'>
-          <p class='w-full text-grey-darkest'>${x.name}</p>
-          <p class='w-full text-grey-darkest'>${x.price}</p>
-   </div>`;
+      commerce.forEach((x,id) => {
+        htmlCode += `<div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${x.name}</h5>
+          <p class="card-text"></p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        <img src="..." class="card-img-bottom" alt="...">
+      </div>`;
 
       });
       listBox.innerHTML = htmlCode;
     }
-
-    
-    function deletecommerce (ind) {
-        commerce.splice(ind, 1);
-        localStorage.setItem("commerce", JSON.stringify(commerce));
-display();
-      }

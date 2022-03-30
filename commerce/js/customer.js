3da1bf6f32fe0b42d70e53df6cf1267a7d645719
rakiafelly <rakiafelly14@gmail.com>
2022-03-30 16:38:35 +0100

    update page client

--- commerce/js/customer.js
+++ commerce/js/customer.js
@@ -1,32 +1,19 @@
 function display() {
-    let htmlCode = "";
-    var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
-  
-    commerce.forEach((x, ind) => {
-      htmlCode += `<div class="card col-3 ">
+  let htmlCode = "";
+  var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
+
+  commerce.forEach((x, ind) => {
+    htmlCode += `<div class="col-4">
+    <div class="card ">
           <div class="card-body">
-          <img src="https://www.generationsforpeace.org/wp-content/uploads/2018/07/empty.jpg" class="img-fluid" alt="...">
+          <img src="${x.image}" class="img-fluid" alt="...">
             <h5 class="card-title">${x.name}</h5>
             <p class="card-text">${x.price} DTN</p>
           </div>
+          </div>
         </div>`;
-    });
-    listBox.innerHTML = htmlCode;
-  }
-  
-  
-  function deletecommerce(ind) {
-    var commerce = JSON.parse(localStorage.getItem("commerce") || "[]");
-   commerce.splice(ind, 1);
-    localStorage.setItem("commerce", JSON.stringify(commerce));
-    display();
-  }
-  
-  function gotoupdate(ind){
-    localStorage.setItem("index", JSON.stringify(ind));
-    window.location.href="update.html"
-  
-  
-  }
-  
-  
+  });
+  listBox.innerHTML = htmlCode;
+}
+
+
